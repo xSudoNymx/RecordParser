cb8000ffeb36c62052909f65ebc08d6adacc1ddf
xSudoNymx <tharri29@gmail.com>
2020-06-01 18:26:37 -0500

    Initial commit

---
+++ src/main/java/com/example/api/model/record.java
@@ -0,0 +1,11 @@
+package com.example.api.model;
+
+import java.util.Date;
+
+public class record {
+    String firstName;
+    String lastName;
+    String gender;
+    String favoriteColor;
+    Date dateOfBirth;
+}
---
+++ src/main/java/com/example/api/util/parser.java
@@ -0,0 +1,10 @@
+package com.example.api.util;
+
+public class parser {
+
+    public static void main(String[] args) {
+        for(String s : args)
+            System.out.println(s);
+    }
+
+}
